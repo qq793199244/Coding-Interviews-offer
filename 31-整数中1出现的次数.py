c9d98eab71663b3a11c3e72e2a0d00172ea23de2
unknown <793199244@qq.com>
2021-03-01 08:57:29 +0800

    数学

---
+++ 31-整数中1出现的次数.py
@@ -0,0 +1,41 @@
+'''
+题目描述
+求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？
+为此他特别数了一下1~13中包含1的数字有1、10、11、12、13因此共出现6次,但是对于后面问题他就没辙了。
+ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）。
+示例1
+输入  13
+返回值 6
+'''
+
+class Solution:
+    # 时间复杂度O(logn)，空间复杂度O(1)
+    def NumberOf1Between1AndN_Solution(self, n):
+        digit, res = 1, 0
+        high, cur, low = n // 10, n % 10, 0
+        while high != 0 or cur != 0:
+            if cur == 0:
+                res += high * digit
+            elif cur == 1:
+                res += high * digit + low + 1
+            else:
+                res += (high + 1) * digit
+            low += cur * digit
+            cur = high % 10
+            high //= 10
+            digit *= 10
+        return res
+
+
+if __name__ == '__main__':
+    u = Solution()
+    print(u.NumberOf1Between1AndN_Solution(13))
+    print(u.NumberOf1Between1AndN_Solution(100))
+
+
+'''
+大佬题解
+https://leetcode-cn.com/problems/1nzheng-shu-zhong-1chu-xian-de-ci-shu-lcof/solution/mian-shi-ti-43-1n-zheng-shu-zhong-1-chu-xian-de-2/
+'''
+
+    
