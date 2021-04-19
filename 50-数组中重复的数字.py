de9c7c2af2a0690a1c555c5866780aefbc1cd7f9
unknown <793199244@qq.com>
2021-04-19 19:41:36 +0800

    平台更新题目描述

--- 50-数组中重复的数字.py
+++ 50-数组中重复的数字.py
@@ -27,28 +27,34 @@ class Solution:
         return False
 
 
-    '''
-    优化，不需要额外的数组，直接在原数组上面操作；
-    和上面的思路不同的是遇到2时就把A[2]-n,这样当再遇到2时，A[2]<0就可以知道2之前遇到过。
-    因为会修改数组的值，所以在查找位置的时候，如果遇到小于0就先加上n再查找位置。
-    '''
+'''
+牛客网更新
+'''
+#题目描述
+# 在一个长度为n的数组里的所有数字都在0到n-1的范围内。
+# 数组中某些数字是重复的，但不知道有几个数字是重复的。
+# 也不知道每个数字重复几次。请找出数组中任一一个重复的数字。
+# 例如，如果输入长度为7的数组[2,3,1,0,2,5,3]，那么对应的输出是2或者3。存在不合法的输入的话输出-1
+# 输入 [2,3,1,0,2,5,3]
+# 返回值 2或3
 
-    def duplicate2(self, numbers, duplication):
+# 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+# @param numbers int整型一维数组
+# @return int整型
+#
+class Solution1:
+    def duplicate(self, numbers):
         # write code here
         n = len(numbers)
-        if n == 0:
-            duplication[0] = -1
-            return False
+        if n == 0 or n == 1:
+            return -1
+        d = {}
         for num in numbers:
-            if num < 0:
-                num += n
-            if numbers[num] >= 0:
-                numbers[num] -= n
-            else:
-                duplication[0] = num
-                return True
-        duplication[0] = -1
-        return False
+            d[num] = d.get(num, 0) + 1
+        for num in numbers:
+            if d[num] > 1:
+                return num
+        return -1
 
 
 if __name__ == '__main__':
@@ -60,16 +66,17 @@ if __name__ == '__main__':
     nums4 = [1, 2, 3, 4]
     nums5 = [1, 0, 1, 0, 2, 3, 4]
     nums6 = [2, 1, 3, 0, 4]
-    # print(u.duplicate(nums1, duplication), "| duplication[0]=", duplication[0])
-    # print(u.duplicate(nums2, duplication), "| duplication[0]=", duplication[0])
-    # print(u.duplicate(nums3, duplication), "| duplication[0]=", duplication[0])
-    # print(u.duplicate(nums4, duplication), "| duplication[0]=", duplication[0])
-    # print(u.duplicate(nums5, duplication), "| duplication[0]=", duplication[0])
-    # print(u.duplicate(nums6, duplication), "| duplication[0]=", duplication[0])
+    print(u.duplicate(nums1, duplication), "| duplication[0]=", duplication[0])
+    print(u.duplicate(nums2, duplication), "| duplication[0]=", duplication[0])
+    print(u.duplicate(nums3, duplication), "| duplication[0]=", duplication[0])
+    print(u.duplicate(nums4, duplication), "| duplication[0]=", duplication[0])
+    print(u.duplicate(nums5, duplication), "| duplication[0]=", duplication[0])
+    print(u.duplicate(nums6, duplication), "| duplication[0]=", duplication[0])
 
-    print(u.duplicate2(nums1, duplication), "| duplication[0]=", duplication[0])
-    print(u.duplicate2(nums2, duplication), "| duplication[0]=", duplication[0])
-    print(u.duplicate2(nums3, duplication), "| duplication[0]=", duplication[0])
-    print(u.duplicate2(nums4, duplication), "| duplication[0]=", duplication[0])
-    print(u.duplicate2(nums5, duplication), "| duplication[0]=", duplication[0])
-    print(u.duplicate2(nums6, duplication), "| duplication[0]=", duplication[0])
+    v = Solution1()
+    print(v.duplicate(nums1))
+    print(v.duplicate(nums2))
+    print(v.duplicate(nums3))
+    print(v.duplicate(nums4))
+    print(v.duplicate(nums5))
+    print(v.duplicate(nums6))
